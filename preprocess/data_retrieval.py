8f5cf95b1bd862df51f4ddba897945a9c14de321
seth22 <sethholtzman101@gmail.com>
2023-10-05 23:03:32 -0400

    Added preliminary preprocess module

---
+++ preprocess/data_retrieval.py
@@ -0,0 +1,56 @@
+import os 
+from PIL import Image
+
+sorted_images_dir = None
+
+#Code below written by ChatGPT with slight modification
+#takes input directory of images and sorts them by row into output directory
+#filetype defaults to .jpeg from camera images 
+
+def sort_images(input_dir, output_dir,filetype=".jpeg"):
+    # Create the output directory if it doesn't exist
+    
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Define the folder names for each row
+    row_folders = ["0", "1", "7", "8", "9", "l", "m", "k", "p", "n"]
+    counter = 1;
+    
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(filetype):
+            input_path = os.path.join(input_dir, filename)
+
+            # Open the image using Pillow
+            image = Image.open(input_path)
+
+            # Get the dimensions of the image
+            width, height = image.size
+
+            # Calculate the size of each small square (assuming perfect square)
+            square_size = width // 10
+
+            # Create folders for each row
+            for folder_name in row_folders:
+                row_folder = os.path.join(output_dir, folder_name)
+                os.makedirs(row_folder, exist_ok=True)
+
+            # Divide the image into 10x10 squares and place each row in the corresponding folder
+            for i in range(10):
+                for j in range(10):
+                    # Crop a square from the image
+                    left = j * square_size
+                    upper = i * square_size
+                    right = left + square_size
+                    lower = upper + square_size
+
+                    square = image.crop((left, upper, right, lower))
+
+                    # Determine the folder for this row and save the square there
+                    row_index = i
+                    row_folder = os.path.join(output_dir, row_folders[row_index])
+                    square.save(os.path.join(row_folder, f"{counter}.png"))
+                    counter += 1
+
+
+    sorted_images_dir = output_dir #adds location for later use
---
+++ preprocess/exception.py
@@ -0,0 +1,8 @@
+class input_dir_empty(Exception):
+    pass
+
+class file_number_exception(Exception):
+    pass
+
+class file_type_exception(Exception):
+    pass
