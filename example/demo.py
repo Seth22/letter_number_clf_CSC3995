00ae7c58cfa466ec84fbfe48227a101773ff1675
seth22 <sethholtzman101@gmail.com>
2023-10-06 15:45:18 -0400

    created prelim demo.py

--- example/demo.py
+++ example/demo.py
@@ -1,1 +1,6 @@
+import pipelines
+import os
 
+os.mkdir("letter_number_clf_CSC3995/example/demo_output")
+
+pipelines.make_model("letter_number_clf_CSC3995/example/org_input_images","letter_number_clf_CSC3995/example/demo_output")
