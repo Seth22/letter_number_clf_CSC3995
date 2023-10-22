f2e18902253828d0d3106e3799c5242c1a2b6ddb
seth22 <sethholtzman101@gmail.com>
2023-10-22 16:50:10 -0400

    moved project files into src/ directory

---
+++ src/example/demo.py
@@ -0,0 +1,16 @@
+import os
+import logging
+
+from src.pipelines import pipeline
+
+logging.basicConfig(
+    level=logging.INFO,  # Set the log level to INFO
+    format='%(levelname)s - %(message)s'  # Customize the log message format
+)
+
+#needed to avoid werid cannot write error
+pardir = os.getcwd()
+
+os.makedirs(f"{pardir}/demo_output", exist_ok=True)
+
+pipeline.make_model(f"{pardir}/org_input_images", f"{pardir}/demo_output", epochs=5)
